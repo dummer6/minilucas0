3cecc79939c4d8a7e776167262ddc0f62d73e5f0
dummer6 <68161707+dummer6@users.noreply.github.com>
2020-07-14 15:46:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,19 +8,32 @@ bot.on('ready',()=> {
 });
 
   
-process.env.tokenclient.on("message", async message => {
+{ bot.on("message", async message => {
+ 
+    if (message.content.startsWith('Sarah')){
+            message.channel.send("Cade ela S2??")}
 
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
-    if(!message.content.startsWith(config.prefix)) return;
+    if (message.content.startsWith('Eduardo')){
+            message.channel.send("Deve estar atras da Yasmim")}
+    
+    if (message.content.startsWith('Bruno')){
+            message.channel.send("É um talarico de merda")}
 
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const comando = args.shift().toLowerCase();
-  
-  // coamdno ping
-  if(comando === "ping") {
-    const m = await message.channel.send("Ping?");
-    m.edit(`Pong! A Latência é ${m.createdTimestamp - message.createdTimestamp}ms. A Latencia da API é ${Math.round(client.ping)}ms`);
-  }
-  
-});
+    if (message.content.startsWith('Tadeu')){
+            message.channel.send("Nada contra mas prefiro distancia ;-;")}
+    
+    if (message.content.startsWith('João')){
+            message.channel.send("Muito umilde mas que todo mundo aqui")}
+    
+    if (message.content.startsWith('Lucas gado')){
+            message.channel.send("Tá loco quer um minimonocelha")}
+
+    if (message.content.startsWith('Me da um pedaço')){
+            message.channel.send("Pede para a professora de etica igual ao Pedrão kkkk")}
+   
+    if (message.content.startsWith('Rafael')){
+           message.channel.send("Cade ele?? Não to vendo ele é muito pequeno ")}
+     
+   if (message.content.startsWith('Gustavo')){
+            message.channel.send("Não entendo nada que ele manda porque ele é adalfabeto")}
+})}
