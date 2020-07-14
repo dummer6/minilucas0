5b81edda57254dc15328f09fec28e8462ac18d31
dummer6 <68161707+dummer6@users.noreply.github.com>
2020-07-14 19:17:31 -0300

    First

--- commands/clear.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (bot,msg) =>{
-    const channel   = msg.channel;
-    const FetchMsg  = await channel.fetchMessages();
-    await channel.bulkDelete(FetchMsg);
-    msg.reply("Chat Limpo!");
-}
--- commands/ping.js
+++
@@ -1,3 +0,0 @@
-module.exports = (bot,msg) =>{
-    msg.reply("Pong");
-}
