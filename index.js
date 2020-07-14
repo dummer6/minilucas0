db3eac4990d5a5c8272a25a1719851fbda04d99b
dummer6 <68161707+dummer6@users.noreply.github.com>
2020-07-14 11:46:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,26 +8,12 @@ bot.on('ready',()=> {
 });
 
   
-   
-bot.on('message', message => {
-    let responseObject = {
-        "Sarah" : "Cade ela S2",
-        "Eduardo" : "Deve estar atras da Yasmim",
-        "Lucas gado" : "Ta locão, ta afim de tomar uma minimonocelha",
-        "Rafael" : "Cd ele, não to vendo ele é muito pequeno",
-        "Tadeu" : "Nada contra mas prefiro distancia",
-        "Bruno" : "Aquele talarico de merda ?",
-        "Gustavo" : "Eu não entendo nada que ele digita porque ele é adalfabeto",
-        "Me da um pedacinho" : "Peda para aprofessora de Etica igual aou Pedrão kkkk",
-        "João" : "Cara muito umilde e top",
-        
-    };
-})
+
 
 bot.on('message', message => {
 if (message.content.startsWith("!roleta")){
     randomNumber = Math.floor(Math.random() * (2 - 1) + 1);
-if(randomNumber == 2){
+if(randomNumber == 1){
     message.reply("Se fudeu:(" )
 } 
 
