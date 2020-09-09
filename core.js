c1a033191d948ee746b32114d0dac15059699065
dummer6 <68161707+dummer6@users.noreply.github.com>
2020-09-09 16:04:31 -0300

    Update core.js

--- core.js
+++ core.js
@@ -30,7 +30,7 @@ if (responseObject[message.content]){
     }
 })
 
-bot.on("message", message =>   {
+bot.on("message", (msg) =>   {
     if(!msg.author.bot){
         const args = msg.content.split(" ");
         if(commands[args[0]]) commands[args[0]](bot,msg);
