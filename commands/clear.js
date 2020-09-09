ce8193dbc83abd2b8789c566bf338d5cffa76794
dummer6 <68161707+dummer6@users.noreply.github.com>
2020-09-09 14:44:51 -0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,6 +1,6 @@
 module.exports = async (bot,msg) =>{
     const channel   = msg.channel;
-    const FetchMsg  = await channel.fetchMessages();
-    await channel.bulkDelete(FetchMsg);
-    msg.reply("Chat Limpo!");
+    const FetchMsg  = await channel.fetchMessages(100000);
+    await channel.bulkDelete(FetchMsg );
+    msg.reply("Vapo Vapo!!!");
 }
